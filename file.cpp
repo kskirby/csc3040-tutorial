5c60ab38f81db82b5afbe4d3e30df94f6ad9e88c
Kendall Kirby <kskirby42@students.tntech.edu>
2017-10-09 12:38:20 -0500

    nope

---
+++ file.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+void output(vector<string> to_print)
+{
+  iter print_iterator = to_print.iterator()
+  while (print_iterator.next())
+  {
+      cout print_iterator;
+  }
+
+}
+
+int main()
+{
+
+  string var1 = "Hello, ";
+  string var2 = "world";
+  string var3 = "!\n";
+  vector<string> write_this;
+  write_this.push_back(var1);
+  write_this.push_back(var2);
+  write_this.push_back(var3);
+  output(write_this);
+}
