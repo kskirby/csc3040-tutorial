545f817863633ab4607530a6ee4e7e755a42767c
Kendall Kirby <kskirby42@students.tntech.edu>
2017-10-09 12:18:34 -0500

    testing

---
+++ testing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+void output(vector<string> to_print)
+{
+  for (int x=0; x<to_print.size(); x++)
+  {
+    cout << to_print[x];
+  }
+
+}
+
+int main()
+{
+
+  string var1 = "Hello, ";
+  string var2 = "world";
+  string var3 = "!\n";
+  vector<string> write_this;
+  write_this.push_back(var1);
+  write_this.push_back(var2);
+  write_this.push_back(var3);
+  output(write_this);
+}
